d8d4362a449fa06e51582567ec0f9062d0143ab7
Safayet <hossainsafayet187@gmail.com>
2024-08-12 23:54:33 +0600

    change in image upload section

--- src/components/Tours.js
+++ src/components/Tours.js
@@ -14,7 +14,6 @@ import { TbCurrencyTaka } from "react-icons/tb";
 import { Link } from "react-router-dom";
 import { HashLink } from "react-router-hash-link";
 
-
 import axios from "axios";
 import { base_url } from "../utils/base_url";
 import ChangeDateFormat from "./ChangeDateFormat";
@@ -22,11 +21,10 @@ import ChangeDateFormat from "./ChangeDateFormat";
 const Tours = () => {
   const [tourItem, setTourItem] = useState([]);
 
-
   useEffect(() => {
     axios
       .get(`${base_url}tour/all-tour`)
-      .then((response) => { 
+      .then((response) => {
         setTourItem(response.data);
       })
       .catch((error) => {
@@ -43,36 +41,36 @@ const Tours = () => {
               <Col key={index} className="mb-5" lg={4} md={6} sm={12}>
                 <div className="d-flex justify-content-center">
                   <div class="tourCard" style={{ maxWidth: "368px" }}>
-                    <img src={item.image} alt="Avatar" className="tourCardImage" />
+                    <img
+                      src={item.image}
+                      alt="Avatar"
+                      className="tourCardImage"
+                    />
                     <div class="tourContainer">
-                      <h4 className="mt-2 text-xl font-semibold">
-                        {item.title}
-                      </h4>
-                      
-                      <div className="d-flex justify-content-between">
-                        <p>Date : </p>
-                        <h5 className="flex">
-                          {ChangeDateFormat(item.date)}{" "}
+                      <h4 className="mb-2 text-primary">{item.title}</h4>
+                      <div className="d-flex justify-content-between mt-2">
+                        <p className="mb-1 text-muted">Date:</p>
+                        <h5 className="mb-1">{ChangeDateFormat(item.date)}</h5>
+                      </div>
+                      <div className="d-flex justify-content-between mt-2">
+                        <p className="mb-1 text-muted">Duration:</p>
+                        <h5 className="mb-1">{item.duration}</h5>
+                      </div>
+                      <div className="d-flex justify-content-between align-items-center mt-2">
+                        <p className="mb-0 text-muted">Starting from:</p>
+                        <h5 className="mb-0 d-flex align-items-center">
+                          {item.price} <TbCurrencyTaka className="ml-1" />
                         </h5>
                       </div>
-                      <div className="d-flex justify-content-between">
-                        <p>Duration : </p>
-                        <h5 className="flex">
-                          {item.duration}{" "}
-                        </h5>
-                      </div>
-                      <div className="d-flex justify-content-between">
-                        <p>Starting from </p>
-                        <h5 className="flex">
-                          {item.price} <TbCurrencyTaka />{" "}
-                        </h5>
-                      </div>
+
                       <HashLink
                         smooth
                         to={`/details/${item._id}#top`}
                         className="tourCardLink mb-2"
                       >
-                        <Button className="btn-warning mb-4">Details</Button>
+                        <Button className=" mt-4 btn-warning mb-4">
+                          Details
+                        </Button>
                       </HashLink>
                     </div>
                   </div>
